867a3e8108c26d920dc66c9150a303a37a993917
F. Waskito <fajar.waskito10@gmail.com>
2024-07-01 00:01:22 +0700

    add src/books.js

---
+++ src/books.js
@@ -0,0 +1,13 @@
+/*
+ * File         : books.js
+ * Project      : bookshelf-api-fwaskito
+ * Author       : F. Waskito
+ * ----------------------------------------------
+ * Created Date : 'Tue, Jun 25th 2024' 9:30:59 AM
+ * Last Modified: 'Tue, Jun 25th 2024' 9:59:34 AM
+ */
+
+
+const books = [];
+
+module.exports = books;
